6d4afb3e4d018c3046e21007253fdfdc61629d41
saaid323 <saaid2538@gmail.com>
2024-01-18 19:05:06 +0000

    added 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,13 +1,14 @@
 -- SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE average FLOAT;
     DECLARE user_name VARCHAR(255);
     DECLARE score_count INT;
-    SELECT SUM(score) INTO average FROM corrections WHERE user_id = user_id;
-    SELECT name INTO user_name FROM users WHERE id = user_id;
-    SELECT COUNT(*) INTO score_count FROM corrections WHERE corrections.user_id = user_id;
-    INSERT INTO users (id, name, average_score) VALUES (user_id, user_name, (average / score_count)) ON DUPLICATE KEY UPDATE average_score = average;
+    SELECT SUM(score) INTO average FROM corrections WHERE corrections.user_id = user_id;
+    SELECT name INTO user_name FROM users WHERE users.id = user_id;
+    SELECT COUNT(score) INTO score_count FROM corrections WHERE corrections.user_id = user_id;
+    INSERT INTO users (id, name, average_score) VALUES (user_id, user_name, (average / score_count)) ON DUPLICATE KEY UPDATE average_score = average / score_count;
 END //
 DELIMITER ;
