34b61dcc06213b81954cf95872630d18be977df1
saaid323 <saaid2538@gmail.com>
2024-01-22 11:12:11 -0800

    added 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-''' '''
+'''script that provides some stats about Nginx logs stored in MongoDB'''
 from pymongo import MongoClient
 
 
@@ -8,4 +8,11 @@ if __name__ == "__main__":
     mydb = myclient["logs"]
     mycol = mydb["nginx"]
     print(mycol.find_one())
-    print(mycol.count_documents({}))
+    print(mycol.count_documents({}), 'logs')
+    print('Methods:')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for i in methods:
+        result = mycol.count_documents({'method': i})
+        print(f'\tmethod {i}: {result}')
+    print(mycol.count_documents({'path': '/status'}), 'status check')
+    
