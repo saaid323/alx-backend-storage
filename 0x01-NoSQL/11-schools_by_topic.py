04fb3b4f5f0e298ac07dc024d75ead8793cc1aa8
saaid323 <saaid2538@gmail.com>
2024-01-22 11:02:38 -0800

    added 11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-''' '''
+'''returns the list of school having a specific topic:'''
 
 
 def schools_by_topic(mongo_collection, topic):
-    ''' '''
+    '''function that returns the list of school having a specific topic'''
     document = mongo_collection.find({'topics': topic})
     return document
