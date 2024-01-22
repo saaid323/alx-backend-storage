df64d8477fb7173ca87417582b5e8982c9fe9ff7
saaid323 <saaid2538@gmail.com>
2024-01-22 10:58:32 -0800

    added 6-update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,5 +4,5 @@
 
 def update_topics(mongo_collection, name, topics):
     '''function that changes all topics of a school document based on the name'''
-    documents = mongo_collection.update_one({'name': name},
+    documents = mongo_collection.update({'name': name},
             {'$set': {'topics': topics}})
