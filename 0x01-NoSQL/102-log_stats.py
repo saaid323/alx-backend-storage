df260247bddb17fff24fcbf506aac2e57e6e44cb
saaid323 <saaid2538@gmail.com>
2024-01-23 09:14:01 -0800

    added 102-log_stats.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''script that provides some stats about Nginx logs stored in MongoDB'''
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    myclient = MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["logs"]
+    mycol = mydb["nginx"]
+    print(mycol.find_one())
+    print(mycol.count_documents({}), 'logs')
+    print('Methods:')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for i in methods:
+        result = mycol.count_documents({'method': i})
+        print(f'\tmethod {i}: {result}')
+    print(mycol.count_documents({'path': '/status'}), 'status check')
+    # ips = mycol.aggregate([{'$match': {'_id': 'ip', 'count': {'$sum': 1}}}])
+    ips = mycol.aggregate([{'$group': {'_id': '$ip', 'count': {'$sum': 1}}},
+                         {'$limit': 10}, {'$sort': {'count': -1}}])
+    print('IPs:')
+    for result in ips:
+        print(f'\t{result["_id"]}: {result["count"]}')
