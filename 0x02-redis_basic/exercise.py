5a41c6edaf11f3c1935d11ddc3d4c1339f3bf000
saaid323 <saaid2538@gmail.com>
2024-01-24 12:22:37 +0000

    added exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,7 @@ from typing import Union
 
 class Cache:
     '''classs used to store item in database'''
+
     def __init__(self):
         self._redis = redis.Redis()
         self.flush = self._redis.flushdb()
@@ -17,13 +18,18 @@ class Cache:
         self.flush
         return str(key)
 
-    # def get(self, key: str, fn: Callable=None) -> Union[str, bytes, int, float]:
-        # if fn is not None:
-            # return fn(self._redis.get(key))
-        # return self._redis.get(key)
+    def get(self,
+            key: str,
+            fn: Callable = None) -> Union[str,
+                                          bytes,
+                                          int,
+                                          float]:
+        if fn is not None:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
 
-    # def get_str(self, data: str) -> str:
-        # return self.get(data, lambda d: d.decode("utf-8"))
+    def get_str(self, data: str) -> str:
+        return self.get(data, lambda d: d.decode("utf-8"))
 
-    # def get_int(self, data: str) -> int:
-        # return self.get(data, lambda d: int(d))
+    def get_int(self, data: str) -> int:
+        return self.get(data, lambda d: int(d))
