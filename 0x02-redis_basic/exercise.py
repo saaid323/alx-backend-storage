c70743b0cf6383a3067ca263d6aa76295279152e
saaid323 <saaid2538@gmail.com>
2024-01-24 11:59:16 +0000

    added exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -27,16 +27,3 @@ class Cache:
 
     def get_int(self, data: str) -> int:
         return self.get(data, int)
-
-
-cache = Cache()
-
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
