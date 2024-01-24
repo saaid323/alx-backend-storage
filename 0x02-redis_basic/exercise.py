ad79166145b942697c72bcb5e613040e0cc1fd5e
saaid323 <saaid2538@gmail.com>
2024-01-24 13:42:55 +0000

    added exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,28 +14,28 @@ class Cache:
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = uuid.uuid4()
-        store = self._redis.set(str(key), data)
+        self._redis.set(str(key), data)
         return str(key)
 
-    # def get(self,
-            # key: str,
-            # fn: Callable = None) -> Union[str,
-                                          # bytes,
-                                          # int,
-                                          # float,
-                                          # None]:
-        # if self._redis.exists(key) == False:
-            # return None
-        # data = self._redis.get(key)
-        # if fn:
-            # return fn(data)
-        # return data
+    def get(self,
+            key: str,
+            fn: Callable = None) -> Union[str,
+                                          bytes,
+                                          int,
+                                          float,
+                                          None]:
+        if self._redis.exists(key) == False:
+            return None
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
 
-    # def get_str(self, key: str) -> str:
-        # return self.get(key, fn=lambda d: d.decode("utf-8"))
+    def get_str(self, key: str) -> str:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
 
-    # def get_int(self, key: str) -> int:
-        # return self.get(key, fn=int)
+    def get_int(self, key: str) -> int:
+        return self.get(key, fn=int)
 
 
 # cache = Cache()
