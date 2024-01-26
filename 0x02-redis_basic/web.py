8e61f2f9c1bec3e46ee4c093b3595f0da7b58a36
saaid323 <saaid2538@gmail.com>
2024-01-26 15:46:56 +0000

    added web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,20 +6,20 @@ from typing import Callable
 
 
 def track(method: Callable) -> Callable:
+    '''function that track how many times a particular URL was accessed'''
     cache = redis.Redis()
+
     @wraps(method)
     def wrapper(url) -> str:
         if cache.exists(content):
             value = cache.
             cache.incr('count:{url}')
-            cache.setex('')
+            cache.setex('count:{url}', 10, cache.get('count:{url}'))
 
 
+@track
 def get_page(url: str) -> str:
-    '''function that track how many times a particular URL was accessed '''
+    '''uses the requests module to obtain the HTML content of a particular
+    URL and returns it.'''
     content = requests.get(url).text
-    cache = redis.Redis()
-    if cache.exists(content):
-        cache.incr(f'count:{url}')
-    else:
-        cache.setex('url', 10, 1)
+    return content
